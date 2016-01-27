3129aecd12aafd34561f5e3352bdabf11513145d
kshvakov <shvakov@gmail.com>
2016-01-27 12:16:41 +0300

    added assertions: "greater than" and "less than"

---
+++ src/assertions/less_than_or_equal.sql
@@ -0,0 +1,14 @@
+create or replace function assert.LessThanOrEqual(_expected anyelement, _actual anyelement, _description text default '') returns boolean as $$
+    declare
+        _context text;
+    begin
+
+        GET DIAGNOSTICS _context = PG_CONTEXT;
+
+        IF _actual <= _expected THEN
+            return true;
+        END IF;
+
+        return  assert.Fail(format('Failed asserting that %s is equal to %s or is less than %s.', _actual, _expected, _expected), _description, _context);
+    end;
+$$ language plpgsql;
