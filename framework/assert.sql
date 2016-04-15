324500edaee90b526bcb62e2f42406c2efbdabd7
kshvakov <shvakov@gmail.com>
2016-04-15 17:07:20 +0300

    warn on duplicate test

--- framework/assert.sql
+++ framework/assert.sql
@@ -75,7 +75,7 @@ create or replace function assert.add_test(_namespace name, _procedure name) ret
         BEGIN
             INSERT INTO assert.tests (namespace, procedure) VALUES (_namespace, _procedure);
         EXCEPTION WHEN UNIQUE_VIOLATION THEN
-            RAISE EXCEPTION 'Test "%" already exists', quote_ident(_namespace) || '.' || quote_ident(_procedure);
+            RAISE WARNING 'Test "%" already exists', quote_ident(_namespace) || '.' || quote_ident(_procedure);
         END; 
     end;
 $$ language plpgsql security definer;
