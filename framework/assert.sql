79d498e2dc7b94693cf8c16d64cc94607974eb3a
Kirill Shvakov <shvakov@gmail.com>
2016-05-29 22:00:48 +0300

    remove assert.view_tests

--- framework/assert.sql
+++ framework/assert.sql
@@ -43,7 +43,7 @@ create table assert.results(
 );
 
 create index idx_assert_test_results on assert.results(test_id);
-
+/*
 create or replace view assert.view_tests as 
     SELECT 
         T.test_id,
@@ -67,7 +67,7 @@ create or replace view assert.view_tests as
         ORDER BY R.run_num DESC 
         LIMIT 1
     ) AS R USING(test_id);
-
+*/
     /* src/functions/add_test.sql */
     
 create or replace function assert.add_test(_namespace name, _procedure name) returns void as $$
